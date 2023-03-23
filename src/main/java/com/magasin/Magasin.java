c76d735edb480458cbd10001d7df06b42066c442
Martin Coronini <martin.coronini@le-campus-numerique.fr>
2023-03-23 16:59:27 +0100

    Small modifications

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -9,6 +9,7 @@ class Magasin {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+
             if (!items[i].name.equals("Comté") && !items[i].name.equals("Pass VIP Concert") && !items[i].name.equals("Kryptonite")){
                 if (items[i].quality > 0){
                     items[i].quality = items[i].quality - 1;
